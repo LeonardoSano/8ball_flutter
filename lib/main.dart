e642e1ad50b5143379b859c99a8241bc6959dedd
leonardo <leonardosano1995@gmail.com>
2020-03-26 15:20:27 +0100

    inizializzato progetto

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: null,
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
